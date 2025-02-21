30c20c831c9c2ad82246d75d1ca731df1d9e3cad
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-21 21:35:44 +0530

    started the project | For college purpose

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <>
+            <div className="text-indigo-500 text-center font-extrabold text-4xl">
+                Launching Soon!
+            </div>
+        </>
+    )
+}
