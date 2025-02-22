a71ec9437f02b2f375656cce8df57b484d18e6da
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-22 15:07:18 +0530

    codeblocks added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Intro } from "./components/Intro";
 import { Navbar } from "./components/Navbar";
 import { Code } from "./icons/Code";
 import { File } from "./icons/File";
-import { code8, code9a } from "./PracticalCodes";
+import { code10a, code10b, code11, code12, code13, code14, code15, code16, code17, code18, code19, code20, code8, code9a, code9b } from "./PracticalCodes";
 
 export default function App() {
     return (
@@ -35,6 +35,19 @@ export default function App() {
             <div>
                 <CodeBlock title="AIM-8: Write a Program to illustrate Arithmetic expressions" code={code8} output={output8} />
                 <CodeBlock title="AIM-9: Write a Program to illustrate Arrays. Program-9a:" code={code9a} output={output9a} />
+                <CodeBlock title="AIM-9: Write a Program to illustrate Arrays. Program-9b:" code={code9b} />
+                <CodeBlock title="AIM-10: Write a Program to illustrate functions. Program-10a:" code={code10a} />
+                <CodeBlock title="AIM-10: Write a Program to illustrate functions. Program-10b:" code={code10b} />
+                <CodeBlock title="AIM-11: Write a Program to illustrate constructor & Destructor:" code={code11} />
+                <CodeBlock title="AIM-12: Write a Program to illustrate Object and classes:" code={code12} />
+                <CodeBlock title="AIM-13: Write a Program to illustrate Operator overloading:" code={code13} />
+                <CodeBlock title="AIM-14: Write a Program to illustrate Function overloading:" code={code14} />
+                <CodeBlock title="AIM-15: Write a Program to illustrate Derived classes & Inheritance:" code={code15} />
+                <CodeBlock title="AIM-16: Write a Program to insert and delete and element from the Stack:" code={code16} />
+                <CodeBlock title="AIM-17: Write a Program to insert and delete and element from the Queue:" code={code17} />
+                <CodeBlock title="AIM-18: Write a Program to insert and delete and element from the Linked List:" code={code18} />
+                <CodeBlock title="AIM-19: Write a Program to implement virtual function:" code={code19} />
+                <CodeBlock title="AIM-20: Write a Program to implement friend function:" code={code20} />
             </div>
 
 
