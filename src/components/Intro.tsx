bec27ccdb903c120c17317036302b9521d16f7c8
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-22 11:53:08 +0530

    portfolio link added

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,7 +2,7 @@ export const Intro = () => {
     return (
         <div className="text-center">
             <h1 className="text-white text-3xl md:text-6xl lg:text-6xl font-bold tracking-tight max-w-4xl mx-auto leading-tight">Struggling with practical exam codes? {" "} 
-                <span onClick={() => {window.open("https://www.youtube.com/watch?v=DIJW7rWPkSw")}} className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent decoration-TextCap2 cursor-pointer hover:underline">Mai huu n...</span>
+                <span onClick={() => {window.open("https://shubhhere.vercel.app")}} className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent decoration-TextCap2 cursor-pointer hover:underline">Mai huu n...</span>
             </h1>
         </div>
     )
